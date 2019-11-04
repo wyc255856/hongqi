bc166595bf0a7673457b96c9b17abae9ef58a76f
王云超 <327250924@qq.com>
2019-11-04 14:26:09 +0800

    首次提交

---
+++ app/src/main/java/com/haowei/wyc/hongqicar/MainActivity.java
@@ -0,0 +1,14 @@
+package com.haowei.wyc.hongqicar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
