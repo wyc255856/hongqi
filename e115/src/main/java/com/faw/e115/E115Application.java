33bfd33f848204359d7875b741e285065fcec59a
WIN-3TJ3R917QD8\Administrator <1227862370@qq.com>
2020-01-14 10:45:48 +0800

    no message

--- e115/src/main/java/com/faw/e115/E115Application.java
+++ e115/src/main/java/com/faw/e115/E115Application.java
@@ -5,7 +5,7 @@ import android.app.Application;
 import com.liulishuo.filedownloader.FileDownloader;
 import com.raizlabs.android.dbflow.config.FlowManager;
 
-public class C229Application extends Application {
+public class E115Application extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
