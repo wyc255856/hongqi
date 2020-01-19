54ca63d9b3b15b9ef4c8314ba6be3a8021637a12
WIN-3TJ3R917QD8\Administrator <1227862370@qq.com>
2020-01-19 10:26:50 +0800

    no message

--- hongqi/src/main/java/com/faw/hongqi/ui/C229MainActivity.java
+++ hongqi/src/main/java/com/faw/hongqi/ui/C229MainActivity.java
@@ -11,17 +11,27 @@ import android.widget.Toast;
 
 import com.faw.hongqi.R;
 import com.faw.hongqi.fragment.BaseFragment;
+import com.faw.hongqi.model.VersionModel;
 import com.faw.hongqi.util.FileUtil;
 import com.faw.hongqi.util.FragmentUtil;
 import com.faw.hongqi.util.LogUtil;
+import com.faw.hongqi.util.PhoneUtil;
+import com.faw.hongqi.util.SharedpreferencesUtil;
 import com.faw.hongqi.widget.TabView;
+import com.google.gson.Gson;
 import com.liulishuo.filedownloader.util.FileDownloadUtils;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+
+import static com.faw.hongqi.ui.C229LoadAndUnzipFileActivity.goC229LoadAndUnzipFileActivity;
 
 public class C229MainActivity extends BaseActivity {
     private BaseFragment currentFragment;
@@ -33,7 +43,13 @@ public class C229MainActivity extends BaseActivity {
     protected void initData() {
         LogUtil.logError("path = " + FileUtil.getResPath());
         requestWritePermission();
-//        startActivity(new Intent(C229MainActivity.this,C229LoadAndUnzipFileActivity.class));
+        new Thread() {
+            @Override
+            public void run() {
+                requestGet();
+            }
+        }.start();
+        SharedpreferencesUtil.setVersionCode(C229MainActivity.this,PhoneUtil.getVersionName(C229MainActivity.this));
     }
 
     @Override
@@ -135,5 +151,77 @@ public class C229MainActivity extends BaseActivity {
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, WRITE_PERMISSION);
             }
         }
+    }
+    private void requestGet() {
+        try {
+            String baseUrl = "http://www.haoweisys.com/hs5_admin/index.php?m=home&c=index&a=get_new_info";
+            StringBuilder tempParams = new StringBuilder();
+            String requestUrl = baseUrl + tempParams.toString();
+            URL url = new URL(requestUrl);
+            final HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
+            urlConn.setConnectTimeout(5 * 1000);
+            urlConn.setReadTimeout(5 * 1000);
+            urlConn.setUseCaches(true);
+            urlConn.setRequestMethod("GET");
+            urlConn.setRequestProperty("Content-Type", "application/json");
+            urlConn.addRequestProperty("Connection", "Keep-Alive");
+            urlConn.connect();
+            if (urlConn.getResponseCode() == 200) {
+                final VersionModel versionModel = new Gson().fromJson(streamToString(urlConn.getInputStream()), VersionModel.class);
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        if (SharedpreferencesUtil.getVersionCode(C229MainActivity.this).equals(versionModel.getVersion())){
+                            if (!fileIsExists(FileDownloadUtils.getDefaultSaveRootPath() + File.separator + "horizon"
+                                    + File.separator + "MyFolder" + File.separator + "NewFile"+ File.separator + "images")){
+                                goC229LoadAndUnzipFileActivity(C229MainActivity.this,versionModel,"gone");
+                                SharedpreferencesUtil.setVersionCode(C229MainActivity.this,versionModel.getVersion());
+                            }
+                        }else{
+                            goC229LoadAndUnzipFileActivity(C229MainActivity.this,versionModel,"visible");
+                            SharedpreferencesUtil.setVersionCode(C229MainActivity.this,versionModel.getVersion());
+                        }
+                    }
+                });
+            } else {
+                //
+            }
+            urlConn.disconnect();
+        } catch (Exception e) {
+            Log.e("tag", "------------->" + e.toString());
+        }
+    }
+    /**
+     * 将输入流转换成字符串
+     *
+     * @param is 从网络获取的输入流
+     * @return
+     */
+    public String streamToString(InputStream is) {
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            int len = 0;
+            while ((len = is.read(buffer)) != -1) {
+                baos.write(buffer, 0, len);
+            }
+            baos.close();
+            is.close();
+            byte[] byteArray = baos.toByteArray();
+            return new String(byteArray);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+    //判断文件夹下是否存在该文件
+    public boolean fileIsExists(String strFile) {
+        try {
+            File f = new File(strFile);
+            if (!f.exists()) {
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
     }
 }
