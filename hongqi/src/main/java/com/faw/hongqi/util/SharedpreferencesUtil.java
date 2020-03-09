c35fbcb0e50db82bda5ae2d7cb29b7b28f2d559a
WIN-3TJ3R917QD8\Administrator <1227862370@qq.com>
2020-03-09 17:59:29 +0800

    no message

--- hongqi/src/main/java/com/faw/hongqi/util/SharedpreferencesUtil.java
+++ hongqi/src/main/java/com/faw/hongqi/util/SharedpreferencesUtil.java
@@ -12,7 +12,6 @@ public class SharedpreferencesUtil {
     private static String IS_UPDATA_DB = "is_update_bd";
     static String VERSION_CODE = "version_code";
     static String ISUNZIP = "is_unzip";
-    private static String CAR_MODEL = "carmodel";
 
     public static Boolean isUploadBD(Context context) {
         SharedPreferences sharedPreferences = context.getSharedPreferences(AppUtil.getPackageName(context)
@@ -31,11 +30,10 @@ public class SharedpreferencesUtil {
                 + PREFERENCES_NAME, Activity.MODE_PRIVATE);
         sharedPreferences.edit().putString(VERSION_CODE, versionCode).commit();
     }
-
     public static String getVersionCode(Context context) {
         SharedPreferences sharedPreferences = context.getSharedPreferences(AppUtil.getPackageName(context)
                 + PREFERENCES_NAME, Activity.MODE_PRIVATE);
-        return sharedPreferences.getString(VERSION_CODE, "");
+        return sharedPreferences.getString(VERSION_CODE, "1");
     }
 
     public static void setIsUnzip(Context context, String unzip) {
@@ -43,27 +41,9 @@ public class SharedpreferencesUtil {
                 + PREFERENCES_NAME, Activity.MODE_PRIVATE);
         sharedPreferences.edit().putString(ISUNZIP, unzip).commit();
     }
-
     public static String getIsUnzip(Context context) {
         SharedPreferences sharedPreferences = context.getSharedPreferences(AppUtil.getPackageName(context)
                 + PREFERENCES_NAME, Activity.MODE_PRIVATE);
         return sharedPreferences.getString(ISUNZIP, "");
-    }
-
-    /**
-     * 储存车型参数
-     */
-    public static void setCarModel(Context context, String model) {
-        SharedPreferences sharedPreferences = context.getSharedPreferences(AppUtil.getPackageName(context)
-                + PREFERENCES_NAME, Activity.MODE_PRIVATE);
-        sharedPreferences.edit().putString(CAR_MODEL, model).commit();
-    }
-    /**
-     * 获取车型参数
-     */
-    public static String getCarModel(Context context ) {
-        SharedPreferences sharedPreferences = context.getSharedPreferences(AppUtil.getPackageName(context)
-                + PREFERENCES_NAME, Activity.MODE_PRIVATE);
-        return sharedPreferences.getString(CAR_MODEL, "C229_1");
     }
 }
